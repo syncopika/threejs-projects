02a68600dc4043bee0590d9801b6e58f2433fb2b
nicholas hung <nhung93@outlook.com>
2023-05-17 22:11:58 -0400

    more progress

--- dimsum/index.js
+++ dimsum/index.js
@@ -25,17 +25,28 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xeeeeee);
 scene.add(camera);
 
+const pointLight = new THREE.PointLight(0xffffff, 2.8, 0);
+pointLight.position.set(5, 40, 40);
+pointLight.castShadow = true;
+scene.add(pointLight);
+
+// for 2nd renderer to display selected item
+const renderer2 = new THREE.WebGLRenderer({antialias: true});
+const camera2 = camera.clone();
+camera2.translateZ(-50);
+const scene2 = new THREE.Scene();
+scene2.background = new THREE.Color(0xeeeeee);
+scene2.add(camera2);
+const ptlight2 = pointLight.clone();
+ptlight2.translateZ(-10);
+scene2.add(ptlight2);
+
 //const spotLight = new THREE.SpotLight(0xffffff);
 //spotLight.position.set(0, 40, -10);
 //spotLight.castShadow = true;
 //spotLight.shadow.mapSize.width = 1024;
 //spotLight.shadow.mapSize.height = 1024;
 //scene.add(spotLight);
-
-const pointLight = new THREE.PointLight(0xffffff, 2.8, 0);
-pointLight.position.set(5, 40, 40);
-pointLight.castShadow = true;
-scene.add(pointLight);
 
 //const hemiLight = new THREE.HemisphereLight(0xffffff);
 //hemiLight.position.set(0, 0, 0);
@@ -49,6 +60,7 @@ let startPositions = [];
 let destinationPositions = []; // end positions when moving models
 let currDishIdx = 0;
 let isMoving = false;
+let itemSelected = false;
 const position = new THREE.Vector3(0, 0, 15); // position for current dish
 
 function getModel(modelFilePath, pos){
@@ -56,13 +68,14 @@ function getModel(modelFilePath, pos){
         loader.load(
             modelFilePath,
             function(gltf){
-                /*
                 gltf.scene.traverse((child) => {
+                    if(child.type === "Mesh"){
+                        // clone material because some meshes seem to share the same material otherwise
+                        child.material = child.material.clone();
+                    }
                 });
-                */
-                gltf.scene.position.x = pos.x;
-                gltf.scene.position.y = pos.y;
-                gltf.scene.position.z = pos.z;
+                
+                gltf.scene.position.set(pos.x, pos.y, pos.z);
                 gltf.scene.name = modelFilePath;
                 resolve(gltf.scene);
             },
@@ -100,7 +113,7 @@ Promise.all(loadedModelPromises).then((objects) => {
 
 function update(){
     // move stuff around, etc.
-    if(loadedModels[currDishIdx]) loadedModels[currDishIdx].rotation.y += 0.01;
+    if(loadedModels[currDishIdx]) loadedModels[currDishIdx].rotation.y += 0.002;
     
     if(isMoving){
         // move the objects to their destination via lerp
@@ -131,7 +144,7 @@ function moveDishes(){
         }
     });
     
-    time += 0.0025;
+    time += 0.005;
     
     if(check === destinationPositions.length){
         isMoving = false;
@@ -192,8 +205,6 @@ document.getElementById('right').addEventListener('click', () => {
     loadedModels[currDishIdx].rotation.copy(loadedModels[currDishIdx].originalRotation);
     
     getNextPositionsForward();
-    //console.log(destinationPositions);
-    //console.log(loadedModels.map(x => x.name));
     
     if(currDishIdx > 0){
         currDishIdx--;
@@ -204,22 +215,94 @@ document.getElementById('right').addEventListener('click', () => {
     loadedModels[currDishIdx].rotateX(Math.PI / 10);
 });
 
-/* allow objects in renderer to be 'clickable'
+function containsDimsumItem(mesh){
+    const validItems = ["siumai", "hagao", "chasiubao", "dantat"];
+    for(let x of validItems){
+        if(mesh.name.includes(x)){
+            return true;
+        }
+    }
+    return false;
+}
+
+// allow objects in renderer to be 'clickable'
 renderer.domElement.addEventListener('mousedown', (evt) => {
+    if(itemSelected) return;
+    
     mouse.x = (evt.offsetX / evt.target.width) * 2 - 1;
     mouse.y = -(evt.offsetY / evt.target.height) * 2 + 1;
     raycaster.setFromCamera(mouse, camera);
     
-    const intersects = raycaster.intersectObjects(scene.children, true); // make sure it's recursive
-    intersects.forEach(x => {
-        console.log(x);
-    });
-});*/
+    const intersects = raycaster.intersectObject(scene, true); // make sure it's recursive
+    const selected = intersects.filter(x => containsDimsumItem(x.object));
+    if(selected.length > 0){
+        const s = selected[0];
+        /*
+        s.object.material.wireframe = true;
+        setTimeout(() => s.object.material.wireframe = false, 500);
+        */
+        displaySelectedItemInfo(s.object);
+        itemSelected = true;
+    }
+});
 
+function displaySelectedItemInfo(mesh){
+    document.getElementById('displayCard').style.display = "block";
+    scene2.children.forEach(x => { if(containsDimsumItem(x)){ scene2.remove(x)}});
+    const container = document.getElementById('displayItemRenderer');
+    if(container.children.length == 0){
+        renderer2.setSize(container.clientWidth, container.clientHeight);    
+        container.appendChild(renderer2.domElement);
+    }
+    const meshCopy = mesh.clone();
+    //meshCopy.material = mesh.material.clone();
+    meshCopy.name2 = "selectedItem";
+    meshCopy.position.set(
+        camera2.position.x,
+        0.5,
+        camera2.position.z - 8
+    );
+    scene2.add(meshCopy);
+    
+    // update card text
+    const header = document.getElementById('displayCardHeader');
+    const description = document.getElementById('displayCardDescription');
+    if(mesh.name.includes('siumai')){
+        header.textContent = "siu mai (燒賣)";
+        description.textContent = "a dumpling whose filling consists of pork and shrimp. it is topped with fish eggs sometimes.";
+    }else if(mesh.name.includes('hagao')){
+        header.textContent = "ha gao (蝦餃)";
+        description.textContent = "a dumpling with a translucent wrapper and shrimp filling";
+    }else if(mesh.name.includes('dantat')){
+        header.textContent = "dan ta (蛋撻)";
+        description.textContent = "a custard tart";
+    }else if(mesh.name.includes('chasiubao')){
+        header.textContent = "cha siu bao (叉燒包)";
+        description.textContent = "a bun with a sweet meat filling";
+    }
+}
+document.getElementById('displayCardCloseBtn').addEventListener('click', () => {
+    const currCard = document.getElementById('displayCard').style.display = "none";
+    itemSelected = false;
+});
 
+let t2 = 0.005;
+let rotationAxis = new THREE.Vector3(1, 1, -1); //new THREE.Vector3(-0.2, -0.3, 0.2);
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    
+    if(itemSelected){
+        scene2.children.forEach(x => {
+            if(x.name2 === "selectedItem"){
+                //t2 += 0.001;
+                x.rotateOnAxis(rotationAxis, t2);
+                //if(t2 > 1) t2 = 0;
+            }
+        });
+        renderer2.render(scene2, camera2);
+    }
+    
     update();
 }
 
