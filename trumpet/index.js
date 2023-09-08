a1652faf3d5b3c338ca781a0a96f1cd976049373
nicholas hung <nhung93@outlook.com>
2023-09-07 22:11:13 -0400

    edits

--- trumpet/index.js
+++ trumpet/index.js
@@ -563,60 +563,60 @@ document.getElementById("stop").addEventListener("click", stopAudio);
 // https://stackoverflow.com/questions/57827830/at-what-point-does-analysernode-execute-its-fft
 // https://courses.physics.illinois.edu/phys406/sp2017/NSF_REU_Reports/2005_reu/Real-Time_Time-Domain_Pitch_Tracking_Using_Wavelets.pdf
 function autoCorrelate(buf, sampleRate){
-	// Implements the ACF2+ algorithm
-	let SIZE = buf.length;
-	let rms = 0;
+    // Implements the ACF2+ algorithm
+    let SIZE = buf.length;
+    let rms = 0;
 
-	for(let i = 0; i < SIZE; i++){
-		let val = buf[i];
-		rms += val*val;
-	}
-	rms = Math.sqrt(rms/SIZE);
-	if(rms < 0.01) // not enough signal
-		return -1;
+    for(let i = 0; i < SIZE; i++){
+        let val = buf[i];
+        rms += val*val;
+    }
+    rms = Math.sqrt(rms/SIZE);
+    if(rms < 0.01) // not enough signal
+        return -1;
 
-	let r1 = 0;
+    let r1 = 0;
     let r2 = SIZE-1;
     const thres = 0.2;
-	for(let i = 0; i < SIZE/2; i++)
-		if(Math.abs(buf[i]) < thres){ r1=i; break; }
-	
+    for(let i = 0; i < SIZE/2; i++)
+        if(Math.abs(buf[i]) < thres){ r1=i; break; }
+    
     for(let i= 1; i < SIZE/2; i++)
-		if(Math.abs(buf[SIZE-i]) < thres){ r2=SIZE-i; break; }
+        if(Math.abs(buf[SIZE-i]) < thres){ r2=SIZE-i; break; }
 
-	buf = buf.slice(r1, r2);
-	SIZE = buf.length;
+    buf = buf.slice(r1, r2);
+    SIZE = buf.length;
 
-	const c = new Array(SIZE).fill(0);
-	for(let i = 0; i < SIZE; i++)
-		for(let j = 0; j < SIZE-i; j++)
-			c[i] = c[i] + buf[j]*buf[j+i];
+    const c = new Array(SIZE).fill(0);
+    for(let i = 0; i < SIZE; i++)
+        for(let j = 0; j < SIZE-i; j++)
+            c[i] = c[i] + buf[j]*buf[j+i];
 
-	let d = 0; 
+    let d = 0; 
     while(c[d] > c[d+1]) d++;
-	
+    
     let maxval=-1, maxpos=-1;
     for(let i = d; i < SIZE; i++){
-		if(c[i] > maxval){
-			maxval = c[i];
-			maxpos = i;
-		}
-	}
-	
+        if(c[i] > maxval){
+            maxval = c[i];
+            maxpos = i;
+        }
+    }
+    
     let T0 = maxpos;
-	const x1 = c[T0-1];
+    const x1 = c[T0-1];
     const x2 = c[T0];
     const x3 = c[T0+1];
-	const a = (x1 + x3 - 2*x2)/2;
-	const b = (x3 - x1)/2;
-	if(a) T0 = T0 - b/(2*a);
+    const a = (x1 + x3 - 2*x2)/2;
+    const b = (x3 - x1)/2;
+    if(a) T0 = T0 - b/(2*a);
 
-	return sampleRate/T0;
+    return sampleRate/T0;
 }
 
 function noteFromPitch(frequency){
-	const noteNum = 12 * (Math.log(frequency / 440)/Math.log(2));
-	return Math.round(noteNum) + 69;
+    const noteNum = 12 * (Math.log(frequency / 440)/Math.log(2));
+    return Math.round(noteNum) + 69;
 }
 
 function syncTrumpetToAudio(){
