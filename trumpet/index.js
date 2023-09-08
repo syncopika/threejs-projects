0efbe00429625661dd038aa0ad8f44e66cb1caea
nicholas hung <nhung93@outlook.com>
2023-09-07 21:21:39 -0400

    more stuff

--- trumpet/index.js
+++ trumpet/index.js
@@ -556,58 +556,66 @@ function stopAudio(){
 document.getElementById("stop").addEventListener("click", stopAudio);
 
 // trying pitch detection from imported audio to sync trumpet with
+// TODO: try/explore other methods other than autocorrelation? (if possible)
 // https://github.com/cwilso/PitchDetect/blob/main/js/pitchdetect.js
 // https://stackoverflow.com/questions/69237143/how-do-i-get-the-audio-frequency-from-my-mic-using-javascript
 // https://stackoverflow.com/questions/42614146/web-audio-api-get-correct-frequency
 // https://stackoverflow.com/questions/57827830/at-what-point-does-analysernode-execute-its-fft
 // https://courses.physics.illinois.edu/phys406/sp2017/NSF_REU_Reports/2005_reu/Real-Time_Time-Domain_Pitch_Tracking_Using_Wavelets.pdf
-function autoCorrelate( buf, sampleRate ) {
+function autoCorrelate(buf, sampleRate){
 	// Implements the ACF2+ algorithm
-	var SIZE = buf.length;
-	var rms = 0;
+	let SIZE = buf.length;
+	let rms = 0;
 
-	for (var i=0;i<SIZE;i++) {
-		var val = buf[i];
+	for(let i = 0; i < SIZE; i++){
+		let val = buf[i];
 		rms += val*val;
 	}
 	rms = Math.sqrt(rms/SIZE);
-	if (rms<0.01) // not enough signal
+	if(rms < 0.01) // not enough signal
 		return -1;
 
-	var r1=0, r2=SIZE-1, thres=0.2;
-	for (var i=0; i<SIZE/2; i++)
-		if (Math.abs(buf[i])<thres) { r1=i; break; }
-	for (var i=1; i<SIZE/2; i++)
-		if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
+	let r1 = 0;
+    let r2 = SIZE-1;
+    const thres = 0.2;
+	for(let i = 0; i < SIZE/2; i++)
+		if(Math.abs(buf[i]) < thres){ r1=i; break; }
+	
+    for(let i= 1; i < SIZE/2; i++)
+		if(Math.abs(buf[SIZE-i]) < thres){ r2=SIZE-i; break; }
 
-	buf = buf.slice(r1,r2);
+	buf = buf.slice(r1, r2);
 	SIZE = buf.length;
 
-	var c = new Array(SIZE).fill(0);
-	for (var i=0; i<SIZE; i++)
-		for (var j=0; j<SIZE-i; j++)
+	const c = new Array(SIZE).fill(0);
+	for(let i = 0; i < SIZE; i++)
+		for(let j = 0; j < SIZE-i; j++)
 			c[i] = c[i] + buf[j]*buf[j+i];
 
-	var d=0; while (c[d]>c[d+1]) d++;
-	var maxval=-1, maxpos=-1;
-	for (var i=d; i<SIZE; i++) {
-		if (c[i] > maxval) {
+	let d = 0; 
+    while(c[d] > c[d+1]) d++;
+	
+    let maxval=-1, maxpos=-1;
+    for(let i = d; i < SIZE; i++){
+		if(c[i] > maxval){
 			maxval = c[i];
 			maxpos = i;
 		}
 	}
-	var T0 = maxpos;
-
-	var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
-	var a = (x1 + x3 - 2*x2)/2;
-	var b = (x3 - x1)/2;
-	if (a) T0 = T0 - b/(2*a);
+	
+    let T0 = maxpos;
+	const x1 = c[T0-1];
+    const x2 = c[T0];
+    const x3 = c[T0+1];
+	const a = (x1 + x3 - 2*x2)/2;
+	const b = (x3 - x1)/2;
+	if(a) T0 = T0 - b/(2*a);
 
 	return sampleRate/T0;
 }
 
-function noteFromPitch(frequency) {
-	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2));
+function noteFromPitch(frequency){
+	const noteNum = 12 * (Math.log(frequency / 440)/Math.log(2));
 	return Math.round(noteNum) + 69;
 }
 
