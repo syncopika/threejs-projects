c9ec99b19ec5fd1a2e0a8aefd2f560552962dd2a
Nicholas Hung <nhung93@outlook.com>
2025-09-06 05:13:41 -0400

    adding a non-post-processing outline shader! :D

--- shaders/shaders/outlineShaderPostProcessing.js
+++ shaders/shaders/outlineShaderPostProcessing.js
@@ -1,11 +1,17 @@
-// non-post-processing outline shader
+// https://tympanus.net/codrops/2022/11/29/sketchy-pencil-effect-with-three-js-post-processing/ - super helpful!
+// https://discourse.threejs.org/t/how-to-render-full-outlines-as-a-post-process-tutorial/22674
+// need to use post-processing!
 
-const outlineShader = {
+// https://roystan.net/articles/outline-shader/ -> this one uses Unity and I had trouble trying to translate it to GLSL :(
+// https://discussions.unity.com/t/_maintex_texelsize-whats-the-meaning/459729/2
+
+// https://blog.maximeheckel.com/posts/beautiful-and-mind-bending-effects-with-webgl-render-targets/ - also cool!
+
+const outlineShaderPostProcessing = {
   vertexShader: `
         varying vec2 vUv;
         varying vec3 vNormal;
-    
-        void main() { 
+        void main() {
             vUv = uv;
             vNormal = normal;
             gl_Position = projectionMatrix *
@@ -15,50 +21,46 @@ const outlineShader = {
     `,
     
   fragShader: `
-        // TODO: since this is a non-post-processing shader, I think the behavior might be slightly different from the post-processing way?
-        // or does this already do what we want, just in the context of the texture?
         varying vec2 vUv;
         varying vec3 vNormal;
-        
         uniform vec2 resolution;
         uniform sampler2D diffuse;
         uniform sampler2D depthTexture;
 
-        float getDiffuseValueAtPoint(int x, int y, vec2 textureSize){
+        float getDiffuseValueAtPoint(int x, int y){
           vec2 point = vec2(x, y);
-          vec2 texelSize = vec2(1.0 / textureSize.x, 1.0 / textureSize.y);
-
-          //vec2 uv = gl_FragCoord.xy * texelSize;
+          vec2 texel = vec2(1.0 / resolution.x, 1.0 / resolution.y);
 
           // TODO: explain vUv + texel * point. 
           // I think part of it is texel * point would get us the actual texel coordinates that we want to analyze? maybe?
-          vec4 color = texture2D(diffuse, vUv + texelSize * point);
+          vec4 color = texture2D(diffuse, vUv + texel * point);
 
+          // TODO: explain why we only need/care about one color channel. 
+          // maybe since we're doing an edge detection filter, all channels should probably be the same value? 
+          // but also, there's no grayscaling or binarization going on?
           return color.r;
         }
 
         float combinedSobelValue(){
-            vec2 textureSize = vec2(textureSize(diffuse, 0));
-            
             // kernel definition (in glsl matrices are filled in column-major order)
             const mat3 Gx = mat3(-1, -2, -1, 0, 0, 0, 1, 2, 1);// x direction kernel
             const mat3 Gy = mat3(-1, 0, 1, -2, 0, 2, -1, 0, 1);// y direction kernel
 
             // fetch the 3x3 neighborhood of the fragment
             // first column
-            float tx0y0 = getDiffuseValueAtPoint(-1, -1, textureSize);
-            float tx0y1 = getDiffuseValueAtPoint(-1, 0, textureSize);
-            float tx0y2 = getDiffuseValueAtPoint(-1, 1, textureSize);
+            float tx0y0 = getDiffuseValueAtPoint(-1, -1);
+            float tx0y1 = getDiffuseValueAtPoint(-1, 0);
+            float tx0y2 = getDiffuseValueAtPoint(-1, 1);
 
             // second column
-            float tx1y0 = getDiffuseValueAtPoint(0, -1, textureSize);
-            float tx1y1 = getDiffuseValueAtPoint(0, 0, textureSize);
-            float tx1y2 = getDiffuseValueAtPoint(0, 1, textureSize);
+            float tx1y0 = getDiffuseValueAtPoint(0, -1);
+            float tx1y1 = getDiffuseValueAtPoint(0, 0);
+            float tx1y2 = getDiffuseValueAtPoint(0, 1);
 
             // third column
-            float tx2y0 = getDiffuseValueAtPoint(1, -1, textureSize);
-            float tx2y1 = getDiffuseValueAtPoint(1, 0, textureSize);
-            float tx2y2 = getDiffuseValueAtPoint(1, 1, textureSize);
+            float tx2y0 = getDiffuseValueAtPoint(1, -1);
+            float tx2y1 = getDiffuseValueAtPoint(1, 0);
+            float tx2y2 = getDiffuseValueAtPoint(1, 1);
 
             // gradient value in x direction
             float valueGx = Gx[0][0] * tx0y0 + Gx[1][0] * tx1y0 + Gx[2][0] * tx2y0 +
@@ -77,8 +79,6 @@ const outlineShader = {
         
         void main() {
           float sobelValue = combinedSobelValue();
-          //gl_FragColor = vec4(sobelValue, sobelValue, sobelValue, 1.0);
-          
           sobelValue = smoothstep(0.01, 0.03, sobelValue);
 
           vec4 lineColor = vec4(0.3, 0.3, 0.3, 1.0);
@@ -86,9 +86,8 @@ const outlineShader = {
           if (sobelValue > 0.1) {
               gl_FragColor = lineColor;
           } else {
-              gl_FragColor = vec4(0.0);
+              gl_FragColor = vec4(1.0);
           }
-          
         }
     `,
 };
