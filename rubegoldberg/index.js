8940d2670ab81f091c7a996b348ec8d10140ec3c
nicholas hung <nhung93@outlook.com>
2026-07-05 11:22:15 -0400

    try this?

--- rubegoldberg/index.js
+++ rubegoldberg/index.js
@@ -11,7 +11,7 @@ document.getElementById('showKeyboard').addEventListener('click', () => {
 
 const fov = 60;
 const camera = new THREE.PerspectiveCamera(fov, container.clientWidth / container.clientHeight, 0.01, 1000);
-camera.position.set(-15, 13, 0);
+camera.position.set(-38, 16, 0);
 camera.rotateY(-Math.PI / 2);
 camera.rotateX(-Math.PI / 8);
 const camRotation = new THREE.Quaternion();
@@ -46,6 +46,7 @@ plane.rotateX(-Math.PI / 2);
 plane.receiveShadow = true;
 plane.castShadow = true;
 scene.add(plane);
+
 const planeShape = new CANNON.Plane();
 const groundMat = new CANNON.Material();
 const planeBody = new CANNON.Body({material: groundMat, mass: 0}); // this plane extends infinitely
@@ -56,9 +57,12 @@ world.addBody(planeBody);
 const dominoCannonMat = new CANNON.Material();
 const sphereMat = new CANNON.Material();
 
-const ball = createSphere(0, 19, 13, 1.0);
+const ball = createSphere(0, 32, -1, 1.0);
 const sphereMesh = ball.mesh;
 const sphereBody = ball.body;
+
+// use a variable to track objects in the path of the rube goldberg device that we're interested in having the camera focus on
+let currObjectToFocusOn = sphereMesh;
 
 // rube goldberg things
 const dominoes = [];
@@ -105,6 +109,13 @@ function createDomino(xPos, yPos, zPos){
   const dominoCannonShape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2));
   const dominoCannonMat = new CANNON.Material();
   const dominoCannonBody = new CANNON.Body({material: dominoCannonMat, mass: 0.2});
+  
+  dominoCannonBody.addEventListener('collide', () => {
+    if(currObjectToFocusOn !== dominoMesh){
+      currObjectToFocusOn = dominoMesh;
+    }
+  });
+  
   dominoCannonBody.position.copy(dominoMesh.position);
   dominoCannonBody.addShape(dominoCannonShape);
   world.addBody(dominoCannonBody);
@@ -151,10 +162,14 @@ const dominoPlatform1MeshDim = new THREE.Vector3(1.4, 8.0, 0.2);
 const dominoPlatform1 = createBox(dominoPlatform1MeshDim, 0x00ffcc, 0.0);
 placeAndRotateMesh(dominoPlatform1.mesh, dominoPlatform1.cannonBody, new THREE.Vector3(0, 5.0, 0), new THREE.Vector3(1, 0, 0), Math.PI / 2, scene, world);
 
-// ramp for ball to roll down
-const platform2MeshDim = new THREE.Vector3(1.4, 15, 0.2);
+// ramps for the initial ball to roll down
+const platform2MeshDim = new THREE.Vector3(1.4, 12, 0.2);
 const platform2 = createBox(platform2MeshDim, 0x00ffcc, 0.0);
-placeAndRotateMesh(platform2.mesh, platform2.cannonBody, new THREE.Vector3(0, 11.5, 10.2), new THREE.Vector3(1, 0, 0), Math.PI / 4, scene, world);
+placeAndRotateMesh(platform2.mesh, platform2.cannonBody, new THREE.Vector3(0, 11.5, 10), new THREE.Vector3(1, 0, 0), Math.PI / 4, scene, world);
+
+const platform3MeshDim = new THREE.Vector3(1.4, 12, 0.2);
+const platform3 = createBox(platform3MeshDim, 0xccffcc, 0.0);
+placeAndRotateMesh(platform3.mesh, platform3.cannonBody, new THREE.Vector3(0, 20, 3), new THREE.Vector3(1, 0, 0), -Math.PI / 3.5, scene, world);
 
 const numDominoes = 4;
 const zSeparation = 0.9;
@@ -165,16 +180,22 @@ for(let i = 0; i < numDominoes; i++){
   startZ -= zSeparation;
 }
 
+/*
 const ball2 = createSphere(0, 7, -2.2, 0.4);
-
+const sphereMesh2 = ball2.mesh;
+const sphereBody2 = ball2.body;
+sphereBody2.addEventListener('collide', () => {
+  currObjectToFocusOn = sphereMesh2;
+});
+*/
 
 // seesaw-type thing
 
 // cylinder
-const cylinderGeo = new THREE.CylinderGeometry(0.1, 0.3, 3, 32);
+const cylinderGeo = new THREE.CylinderGeometry(0.3, 0.3, 3, 32);
 const cylinderMat = new THREE.MeshBasicMaterial({color: 0xeeeeee});
 const cylinderMesh = new THREE.Mesh(cylinderGeo, cylinderMat);
-cylinderMesh.position.set(0, 2.1, -6);
+cylinderMesh.position.set(0, 8.5, -1.2);
 cylinderMesh.rotateZ(Math.PI / 2);
 scene.add(cylinderMesh);
 
@@ -197,18 +218,19 @@ cylinderCannonBody.addShape(cylinderCannonShape);
 world.addBody(cylinderCannonBody);
 
 // plank
-const plankGeo = new THREE.BoxGeometry(1.5, 0.02, 3.5);
+const plankGeo = new THREE.BoxGeometry(1.8, 0.02, 2.8);
 const plankMat = new THREE.MeshPhongMaterial({color: 0xffff00});
 const plankMesh = new THREE.Mesh(plankGeo, plankMat);
 plankMesh.castShadow = true;
 plankMesh.receiveShadow = true;
-plankMesh.position.set(0, 2.3, -7);
+plankMesh.position.set(0, 8.7, -2);
 plankMesh.rotateX(145 * (Math.PI / 180)); // 145 deg
 scene.add(plankMesh);
 
-const plankCannonShape = new CANNON.Box(new CANNON.Vec3(1.0, 0.02, 1.9));
+const plankCannonShape = new CANNON.Box(new CANNON.Vec3(0.9, 0.01, 1.4));
 const plankCannonMat = new CANNON.Material();
 const plankCannonBody = new CANNON.Body({material: plankCannonMat, mass: 0.2});
+
 plankCannonBody.position.copy(plankMesh.position);
 plankCannonBody.quaternion.copy(plankMesh.quaternion);
 plankCannonBody.addShape(plankCannonShape);
@@ -220,24 +242,28 @@ const cubeMat = new THREE.MeshPhongMaterial({color: 0xff00ff});
 const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat);
 cubeMesh.castShadow = true;
 cubeMesh.receiveShadow = true;
-cubeMesh.position.set(0, 3.0, -8);
+cubeMesh.position.set(0, 9.2, -3);
 scene.add(cubeMesh);
 
 const cubeCannonShape = new CANNON.Box(new CANNON.Vec3(0.6, 0.6, 0.6));
 const cubeCannonMat = new CANNON.Material();
-const cubeCannonBody = new CANNON.Body({material: cubeCannonMat, mass: 0.5});
+const cubeCannonBody = new CANNON.Body({material: cubeCannonMat, mass: 0.1});
 cubeCannonBody.position.copy(cubeMesh.position);
 cubeCannonBody.quaternion.copy(cubeMesh.quaternion);
 cubeCannonBody.addShape(cubeCannonShape);
 world.addBody(cubeCannonBody);
 
-//const plankContactMat = new CANNON.ContactMaterial(plankCannonMat, dominoCannonMat, {friction: 0.0,  restitution: 0.5});
-//world.addContactMaterial(plankContactMat);
+cubeCannonBody.addEventListener('collide', () => {
+  currObjectToFocusOn = cubeMesh;
+});
 
 const clock = new THREE.Clock();
 let delta;
 
 function update(){
+  camera.position.set(camera.position.x, currObjectToFocusOn.position.y, camera.position.z);
+  camera.lookAt(currObjectToFocusOn.position);
+  
   // move the stuff
   delta = Math.min(clock.getDelta(), 0.1);
   world.step(delta);
@@ -259,6 +285,9 @@ function update(){
   
   cubeMesh.position.copy(cubeCannonBody.position);
   cubeMesh.quaternion.copy(cubeCannonBody.quaternion);
+  
+  //sphereMesh2.position.copy(sphereBody2.position);
+  //sphereMesh2.quaternion.copy(sphereBody2.quaternion);
 }
 
 function keydown(evt){
@@ -288,33 +317,6 @@ function keydown(evt){
   */
 }
 document.addEventListener('keydown', keydown);
-
-function createPole(){  
-  // the pole
-  const poleGeometry = new THREE.CylinderGeometry(0.2, 0.2, 7, 20); // radius top, radius bottom, height, radial segments
-  const poleMaterial = new THREE.MeshBasicMaterial({color: 0xdddddd, wireframe: true});
-  const pole = new THREE.Mesh(poleGeometry, poleMaterial);
-  pole.receiveShadow = true;
-  pole.position.x = 0;
-  pole.position.y = 0;
-  pole.position.z = -9.2;
-  scene.add(pole);
-    
-  const poleIndices = pole.geometry.faces.map(face => [face.a, face.b, face.c]).reduce((x, acc) => acc.concat(x), []);
-  const poleVertices = pole.geometry.vertices.map(vert => [vert.x, vert.y, vert.z]).reduce((x, acc) => acc.concat(x), []);
-  const poleShape = new CANNON.Trimesh(poleVertices, poleIndices);
-  const poleMat = new CANNON.Material();
-  const poleBody = new CANNON.Body({material: poleMat, mass: 0});
-  poleBody.position.x = pole.position.x;
-  poleBody.position.y = pole.position.y;
-  poleBody.position.z = pole.position.z;
-  poleBody.addShape(poleShape);
-  world.addBody(poleBody);
-    
-  const poleContactMat = new CANNON.ContactMaterial(poleMat, sphereMat, {friction: 0.0,  restitution: 0.1});
-  world.addContactMaterial(poleContactMat);
-}
-
 
 function animate(){
   requestAnimationFrame(animate);
